f5d9a0ec4fc6a84b9161420c3f986ac13f538e0a
Tomasz Sodzawiczny <tomasz.sodzawiczny@brainly.com>
2019-02-24 23:13:28 +0100

    Add warnings

--- packages/postcss-modules-extend-from/index.js
+++ packages/postcss-modules-extend-from/index.js
@@ -1,31 +1,57 @@
 const postcss = require('postcss');
 
-let seqNo = -1;
-const getImportAs = function(original) {
-  seqNo++;
-  return '__extends_from_' + seqNo + '_' + original;
-};
-const plugin = postcss.plugin('postcss-modules-extend-from', () => root => {
-  root.walkAtRules(atRule => {
-    if (atRule.name === 'extend') {
-      const match = atRule.params.match(/\s*.(\S+)\s+from\s+(.+)\s*/);
-      if (match) {
-        // TODO sanity checks
-        const importWhat = match[1];
-        const importAs = getImportAs(importWhat);
-        const importFrom = match[2];
+const extendClassRuleName = 'extend-class';
 
-        root.prepend(
-          postcss
-            .rule({ selector: ':import(' + importFrom + ')' })
-            .prepend(postcss.decl({ prop: importAs, value: importWhat }))
-        );
-        atRule.replaceWith(
-          postcss.atRule({ name: 'extend-class', params: importAs })
-        );
+const getImportAs = function(original) {
+  const randomString = Math.random()
+    .toString(36)
+    .substring(2);
+  return `__extends_from_${original}_${randomString}`;
+};
+
+const plugin = postcss.plugin(
+  'postcss-modules-extend-from',
+  () => (root, result) => {
+    root.walkAtRules(atRule => {
+      if (atRule.name === 'extend') {
+        const match = atRule.params.match(/\s*\.(\S+)\s+from\s+(.+)\s*/);
+        if (match) {
+          const importWhat = match[1];
+          const importAs = getImportAs(importWhat);
+          const importFrom = match[2];
+
+          root.prepend(
+            postcss
+              .rule({ selector: ':import(' + importFrom + ')' })
+              .prepend(postcss.decl({ prop: importAs, value: importWhat }))
+          );
+          atRule.replaceWith(
+            postcss.atRule({ name: extendClassRuleName, params: importAs })
+          );
+        } else {
+          const errorMatch = atRule.params.match(/\s*(\S+)\s+from\s+(.+)\s*/);
+          if (errorMatch) {
+            const importWhat = errorMatch[1];
+
+            if (importWhat[0] === '%') {
+              // TODO: support functional selectors https://jonathantneal.github.io/specs/css-extend-rule/#functional-selector
+              atRule.warn(
+                result,
+                '@extends from syntax can only be used with CSS classes - functional selectors (e.g. %foo) are not supported yet',
+                { word: importWhat }
+              );
+            } else {
+              atRule.warn(
+                result,
+                '@extends from syntax can only be used with CSS classes',
+                { word: importWhat }
+              );
+            }
+          }
+        }
       }
-    }
-  });
-});
+    });
+  }
+);
 
 module.exports = plugin;
